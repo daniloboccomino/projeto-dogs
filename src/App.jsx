114ee37cd4ba04eb02db41f488249b1685786260
daniloboccomino <daniloboccomino@gmail.com>
2025-02-23 11:49:00 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -10,46 +10,50 @@ import User from './components/user/User'
 import Photo from './components/photo/Photo'
 import UserProfile from './components/user/UserProfile'
 import NotFound from './components/NotFound'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <>
+    <div className='app'>
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route
-              path='/'
-              element={<Home />}
-            />
-            <Route
-              path='login/*'
-              element={<Login />}
-            />
-            <Route
-              path='conta/*'
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path='foto/:id'
-              element={<Photo />}
-            />
-            <Route
-              path='perfil/:user'
-              element={<UserProfile />}
-            />
-            <Route
-              path='*'
-              element={<NotFound />}
-            />
-          </Routes>
+          <main className='app-body'>
+            <Routes>
+              <Route
+                path='/'
+                element={<Home />}
+              />
+              <Route
+                path='login/*'
+                element={<Login />}
+              />
+              <Route
+                path='conta/*'
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path='foto/:id'
+                element={<Photo />}
+              />
+              <Route
+                path='perfil/:user'
+                element={<UserProfile />}
+              />
+              <Route
+                path='*'
+                element={<NotFound />}
+              />
+            </Routes>
+          </main>
+          <Footer />
         </UserStorage>
       </BrowserRouter>
-    </>
+    </div>
   )
 }
 
